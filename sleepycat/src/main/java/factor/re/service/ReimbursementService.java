a39175fefdcfbbd53f7c836fbb67fc8004e22a15
dannyBernier <46949659+dannyBernier@users.noreply.github.com>
2021-04-04 21:58:09 -0500

    fixed resloving functionality

--- sleepycat/src/main/java/factor/re/service/ReimbursementService.java
+++ sleepycat/src/main/java/factor/re/service/ReimbursementService.java
@@ -29,6 +29,7 @@ public class ReimbursementService {
 		try {
 			Reimbursement r = new Gson().fromJson(json, Reimbursement.class);
 			LOGGER.debug("JSON from the client was successfully parsed.");
+			r.setSubmitted(Timestamp.from(Instant.now()));
 			rd.insert(r);
 			return true;
 		} catch (Exception e) {
